f954eb66caad97d3caa2c6fc21ca934bf2c21ca7
what's for dinner <cruelangeleo7@gmail.com>
2020-10-10 23:36:41 +1100

    WIP getting queue functionality into the common package

--- pkg/orders/go.mod
+++ pkg/orders/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/mediocregopher/radix/v3 v3.5.2
+	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 )
--- pkg/orders/queue.go
+++ pkg/orders/queue.go
@@ -1,1 +1,162 @@
 package orders
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/streadway/amqp"
+)
+
+// OrderQueue manages the connection to RabbitMQ
+type OrderQueue struct {
+	Connection *amqp.Connection
+	QueueName  string
+}
+
+// OrderMessage contains the body of the queue message
+type OrderMessage struct {
+	OrderKey string `json:"orderKey"`
+	Pastry   string `json:"pastry"`
+}
+
+// Connect will connect to the RabbitMQ instance
+func (q *OrderQueue) Connect(host string, username string, password string) error {
+	connectionString := fmt.Sprintf("amqp://%s:%s@%s", username, password, host)
+	connection, err := amqp.Dial(connectionString)
+
+	if err != nil {
+		return err
+	}
+
+	q.Connection = connection
+	return nil
+}
+
+// Disconnect closes the connection to RabbitMQ
+func (q *OrderQueue) Disconnect() error {
+	err := q.Connection.Close()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeclareQueue declares the queue that will be used for processing orders
+func (q *OrderQueue) DeclareQueue() error {
+	if q.Connection == nil {
+		return errors.New("connection to RabbitMQ hasn't been established")
+	}
+
+	channel, err := q.Connection.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	queue, err := channel.QueueDeclare(
+		"orders",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	q.QueueName = queue.Name
+	return nil
+}
+
+// PublishOrderMessage publishes an OrderMessage to the queue
+func (q *OrderQueue) PublishOrderMessage(orderMessage *OrderMessage) error {
+	if q.Connection == nil {
+		return errors.New("connection to RabbitMQ hasn't been established")
+	}
+
+	channel, err := q.Connection.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	messageBody, err := json.Marshal(*orderMessage)
+	err = channel.Publish(
+		"",
+		q.QueueName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        messageBody,
+		},
+	)
+
+	return err
+}
+
+// ConsumeOrderQueue creates a blocking connection to the order queue
+func (q *OrderQueue) ConsumeOrderQueue(processFunction func(*OrderMessage) error, errorFunc func(error)) error {
+	if q.Connection == nil {
+		return errors.New("connection to RabbitMQ hasn't been established")
+	}
+
+	channel, err := q.Connection.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	orders, err := channel.Consume(
+		q.QueueName,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	go func() {
+		for order := range orders {
+			orderMessage := new(OrderMessage)
+			err := json.Unmarshal(order.Body, orderMessage)
+
+			if err != nil {
+				errorFunc(err)
+			}
+
+			err = processFunction(orderMessage)
+
+			if err != nil {
+				errorFunc(err)
+			}
+
+			err = order.Ack(false)
+
+			if err != nil {
+				errorFunc(err)
+			}
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+
+	return nil
+}
