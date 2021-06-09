0c5330d5604c8cb17f2baa63bd8f9394a65ad57c
what's for dinner <cruelangeleo7@gmail.com>
2021-06-09 22:28:24 +1000

    changed config from using flags to env vars to make runtime config a bit cleaner/easier

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,25 +2,47 @@ package config
 
 import (
 	"context"
-	"flag"
+	"os"
 )
 
 // Config represents all the configuration information for the baker and reception
 type Config struct {
-	DBHost         *string
-	RabbitHost     *string
-	RabbitUsername *string
-	RabbitPassword *string
+	DBHost         string
+	RabbitHost     string
+	RabbitUsername string
+	RabbitPassword string
 }
 
 // GetConfig reads in config values from their appropriate command line flags
 func GetConfig(ctx context.Context) *Config {
 	c := new(Config)
-	c.DBHost = flag.String("dbhost", "127.0.0.1:6379", "connection string for Redis DB")
-	c.RabbitHost = flag.String("rabbithost", "127.0.0.1:5672", "host for RabbitMQ")
-	c.RabbitUsername = flag.String("rabbituser", "guest", "username for connecting to RabbitMQ")
-	c.RabbitPassword = flag.String("rabbitpass", "guest", "password for connecting to RabbitMQ")
-	flag.Parse()
+
+	// Set sensible defaults (I.e. testing or a dev environment)
+	c.DBHost = "127.0.0.1:6379"
+	c.RabbitHost = "127.0.0.1:5672"
+	c.RabbitUsername = "guest"
+	c.RabbitPassword = "guest"
+
+	// Source values from the environment
+	val, exists := os.LookupEnv("DB_HOST")
+	if exists {
+		c.DBHost = val
+	}
+
+	val, exists = os.LookupEnv("RABBIT_HOST")
+	if exists {
+		c.RabbitHost = val
+	}
+
+	val, exists = os.LookupEnv("RABBIT_USERNAME")
+	if exists {
+		c.RabbitUsername = val
+	}
+
+	val, exists = os.LookupEnv("RABBIT_PASSWORD")
+	if exists {
+		c.RabbitPassword = val
+	}
 
 	return c
 }
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,3 +1,83 @@
 package config
 
-// No tests at this point because it's just a struct wrapped around the falgs library
+import (
+	"context"
+	"os"
+	"testing"
+)
+
+func TestGetConfig(t *testing.T) {
+	tests := map[string]struct {
+		dbHostOverride         string
+		rabbitHostOverride     string
+		rabbitUsernameOverride string
+		rabbitPasswordOverride string
+	}{
+		"no override":    {"", "", "", ""},
+		"some overrides": {"foo", "", "", "qux"},
+		"all overrides":  {"foo", "bar", "baz", "qux"},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			if test.dbHostOverride != "" {
+				os.Setenv("DB_HOST", test.dbHostOverride)
+			} else {
+				os.Unsetenv("DB_HOST")
+			}
+
+			if test.rabbitHostOverride != "" {
+				os.Setenv("RABBIT_HOST", test.rabbitHostOverride)
+			} else {
+				os.Unsetenv("RABBIT_HOST")
+			}
+
+			if test.rabbitUsernameOverride != "" {
+				os.Setenv("RABBIT_USERNAME", test.rabbitUsernameOverride)
+			} else {
+				os.Unsetenv("RABBIT_USERNAME")
+			}
+
+			if test.rabbitPasswordOverride != "" {
+				os.Setenv("RABBIT_PASSWORD", test.rabbitPasswordOverride)
+			} else {
+				os.Unsetenv("RABBIT_PASSWORD")
+			}
+
+			c := GetConfig(context.Background())
+
+			if test.dbHostOverride != "" && c.DBHost != test.dbHostOverride {
+				t.Fatalf("Override failed.\nExpected: %s\nGot: %s", test.dbHostOverride, c.DBHost)
+			}
+
+			if test.dbHostOverride == "" && c.DBHost != "127.0.0.1:6379" {
+				t.Fatalf("Default failed.\nExpected: %s\nGot: %s", test.dbHostOverride, c.DBHost)
+			}
+
+			if test.rabbitHostOverride != "" && c.RabbitHost != test.rabbitHostOverride {
+				t.Fatalf("Override failed.\nExpected: %s\nGot: %s", test.rabbitHostOverride, c.RabbitHost)
+			}
+
+			if test.rabbitHostOverride == "" && c.RabbitHost != "127.0.0.1:5672" {
+				t.Fatalf("Default failed.\nExpected: %s\nGot: %s", test.rabbitHostOverride, c.RabbitHost)
+			}
+
+			if test.rabbitUsernameOverride != "" && c.RabbitUsername != test.rabbitUsernameOverride {
+				t.Fatalf("Override failed.\nExpected: %s\nGot: %s", test.rabbitUsernameOverride, c.RabbitUsername)
+			}
+
+			if test.rabbitUsernameOverride == "" && c.RabbitUsername != "guest" {
+				t.Fatalf("Default failed.\nExpected: %s\nGot: %s", test.rabbitUsernameOverride, c.RabbitUsername)
+			}
+
+			if test.rabbitPasswordOverride != "" && c.RabbitPassword != test.rabbitPasswordOverride {
+				t.Fatalf("Override failed.\nExpected: %s\nGot: %s", test.rabbitPasswordOverride, c.RabbitPassword)
+			}
+
+			if test.rabbitPasswordOverride == "" && c.RabbitPassword != "guest" {
+				t.Fatalf("Default failed.\nExpected: %s\nGot: %s", test.rabbitPasswordOverride, c.RabbitPassword)
+			}
+
+		})
+	}
+}
